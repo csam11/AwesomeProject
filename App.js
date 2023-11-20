aa2b81741d97a157cbe7b6d73ffa608687faa858
csam11 <csam@uwo.ca>
2023-11-20 14:01:13 -0500

    commit #1 adjustments to project details for start of capstone

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Simple Fitness</Text>
+      <button>Click me!</button>
       <StatusBar style="auto" />
     </View>
   );
