3185bcb95c8dae3ade920e772fc2d31eb261a544
Daniel <danielmaximos2001@gmail.com>
2024-03-18 17:02:25 -0400

    login modal added instead of alert

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,20 +1,23 @@
 // LoginScreen.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Modal } from 'react-native';
 import axios from 'axios';
 
 const LoginScreen = ({ navigation }) => {
+
+  const [showPrompt, setShowPrompt] = useState(false)
+
   const apiCall = () => {
-      axios.post('http://localhost:8080/api/users/login', {
+      axios.post('http://localhost:19006/api/users/login', {
           username: username,
-          password: password
+          password: password,
       })
       .then(response => {
           console.log(response.data.message); // Log the response from the server
           navigation.navigate('WeightJournalScreen');
       })
       .catch(error => {
-          alert('Login failed. ' + error.response.data.error);
+          setShowPrompt(true);
           setUsername('');
           setPassword('');
       });
@@ -42,6 +45,21 @@ const LoginScreen = ({ navigation }) => {
       />
 
       <Button title="Login" onPress={apiCall} />
+
+      <Modal
+        visible={showPrompt}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => setShowPrompt(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>Login failed. Please enter correct username and password, or register for an account.</Text>
+            <Button title="OK" onPress={() => setShowPrompt(false)} />
+          </View>
+        </View>
+      </Modal>
+
     </View>
   );
 };
@@ -60,6 +78,21 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 8,
     width: '80%',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    marginBottom: 20,
   },
 });
 
